f4b572a8ffc57589433e18ad1985bdd668ab253c
papito1992 <paulius.mac92@gmail.com>
2019-01-29 20:00:05 +0200

    Initial commit

---
+++ src/main/java/SpringUnitTesting/SpringUnitTestMain.java
@@ -0,0 +1,28 @@
+package SpringUnitTesting;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SpringUnitTestMain {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUnitTestMain.class,args);
+    }
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx){
+        return args -> {
+            System.out.println("beans provided by spring boot: ");
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName :
+                    beanNames) {
+                System.out.println(beanName);
+            }
+        };
+    }
+}
